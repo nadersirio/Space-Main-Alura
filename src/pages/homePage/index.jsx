2fef18ec3bf91ae07a82864b754199ea172beef6
Náder W. S. Sírio <nadersirio@gmail.com>
2023-06-16 22:31:05 -0300

    Revert "att-1.3"
    This reverts commit b756a641e018ffadbf9b8e24c7807fb7c9da0b6b.

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,13 +1,21 @@
+<<<<<<< HEAD
 import MainPageBanner from "../MainPageBanner";
 import Footer from "../footer";
 import { Header } from "../header/index";
+=======
+import { Header } from "../header/index"
+>>>>>>> parent of b756a64 (att-1.3)
 
 export const HomePage = () => {
   return (
     <>
       <Header />
+<<<<<<< HEAD
       <MainPageBanner />
       <Footer />
+=======
+      <h1> oie </h1>
+>>>>>>> parent of b756a64 (att-1.3)
     </>
   )
 }
